06e1315fff49dfb46f8c2620741dd208e07a9cc0
Jens Spanier <jens@spanier.es>
2023-12-11 15:34:52 +0100

    Add 2023 11

---
+++ 2023/11/task1.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once __DIR__ . '/../../common/common.php';
+
+$lines = getInputLines(__DIR__ . '/input.txt');
+
+$map = [];
+$emptyRows = [];
+$emptyColumns = [];
+
+foreach ($lines as $y => $line) {
+    $splittedLine = str_split($line);
+    $galaxiesCount = 0;
+    foreach ($splittedLine as $x => $char) {
+        if ($char === '#') {
+            $galaxiesCount++;
+        }
+        $map[$y][$x] = $char;
+    }
+
+    if ($galaxiesCount === 0) {
+        $emptyRows[] = $y;
+    }
+}
+
+for ($x = 0; $x < count($map[0]); $x++) {
+    $galaxiesCount = 0;
+    for ($y = 0; $y < count($map); $y++) {
+        if ($map[$y][$x] === '#') {
+            $galaxiesCount++;
+        }
+    }
+
+    if ($galaxiesCount === 0) {
+        $emptyColumns[] = $x;
+    }
+}
+
+$emptyRows = array_reverse($emptyRows);
+$emptyColumns = array_reverse($emptyColumns);
+
+foreach ($emptyRows as $emptyRow) {
+    array_splice($map, $emptyRow, 0, [array_fill(0, count($map[0]), '.')]);
+}
+
+foreach ($emptyColumns as $emptyColumn) {
+    for ($y = 0; $y < count($map); $y++) {
+        array_splice($map[$y], $emptyColumn, 0, ['.']);
+    }
+}
+
+$galaxyPositions = [];
+
+for ($y = 0; $y < count($map); $y++) {
+    for ($x = 0; $x < count($map[0]); $x++) {
+        if ($map[$y][$x] === '#') {
+            $galaxyPositions[] = [$x, $y];
+        }
+    }
+}
+
+$sum = 0;
+
+for ($i = 0; $i < count($galaxyPositions); $i++) {
+    [$xStart, $yStart] = $galaxyPositions[$i];
+
+    for ($j = $i + 1; $j < count($galaxyPositions); $j++) {
+        [$xEnd, $yEnd] = $galaxyPositions[$j];
+
+        $horizontal = abs($xStart - $xEnd);
+        $vertical = abs($yStart - $yEnd);
+
+        $sum += $horizontal + $vertical;
+    }
+}
+
+var_dump($sum);
